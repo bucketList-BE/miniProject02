8e2d1844f170be9c3d9df704ca4450893995ece9
journeypark93 <qwe1781@naver.com>
2022-04-09 20:54:47 +0900

    entity 생성

--- src/main/java/com/sparta/bucket/controller/PostController.java
+++ src/main/java/com/sparta/bucket/controller/PostController.java
@@ -1,4 +1,45 @@
 package com.sparta.bucket.controller;
 
+import com.sparta.bucket.dto.PostDto;
+import com.sparta.bucket.dto.ResponsePostDto;
+import com.sparta.bucket.model.User;
+import com.sparta.bucket.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+//@ResponseStatus(HttpStatus.OK)
 public class PostController {
+
+    private final PostService postService;
+
+    //게시글 작성
+    @PostMapping("/api/posts")
+    public ResponsePostDto createPost(
+            @RequestBody List<PostDto> postDtos,
+//            @AuthenticationPrincipal UserDetailsImpl userDetails
+            User user
+    ) {
+        return postService.registerPost(postDtos, user);//userDetails.getUser);
+    }
+
+    //게시글 조회
+    @GetMapping("/api/post/{postId}")
+    public PostDto findPost(
+            @PathVariable Long postId
+    ) {
+        return postService.findPost(postId);
+    }
+
+    //게시글 수정
+    @PutMapping("/api/post/{postId}")
+    public PostDto updatePost(
+            @PathVariable Long postId,
+            @RequestBody PostDto postDtos
+    ) {
+        return postService.updatePost(postId, postDtos);
+    }
 }
---
+++ src/main/java/com/sparta/bucket/model/Todo.java
@@ -0,0 +1,35 @@
+package com.sparta.bucket.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Todo {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Boolean done;
+
+    @ManyToOne
+    @JoinColumn(name="POST_ID", nullable = false)
+    private Post post;
+
+
+    public Todo(String content, Boolean done, Post post){
+        this.content = content;
+        this.done = done;
+        this.post = post;
+    }
+}
